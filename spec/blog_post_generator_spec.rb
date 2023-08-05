18e35bbed9acc8c2ed149fe98cb90a54f2dee7b7
Vasili Cekaskin <vasili.cekaskin@invision.de>
2023-08-05 02:38:18 +0200

    Configure prettier

--- spec/blog_post_generator_spec.rb
+++ spec/blog_post_generator_spec.rb
@@ -1,9 +1,9 @@
 require_relative '../lib/blog_post_generator'
 
 RSpec.describe BlogPostGenerator do
-	subject(:blog_post_generator) { BlogPostGenerator.new }
+  subject(:blog_post_generator) { BlogPostGenerator.new }
 
-	describe '#generate' do
-		expect(blog_post_generator.generate).to be_truthy
-	end
+  describe '#generate' do
+    expect(blog_post_generator.generate).to be_truthy
+  end
 end
