7248afec24963cad2f8282ae194b6bb1ef2bf592
Vasili Cekaskin <vasili.cekaskin@invision.de>
2023-08-06 16:37:17 +0200

    Add test suite for disk writer

---
+++ spec/app/blog_post_generator/blog_post_writer/disk_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../../../../lib/app/blog_post_generator/blog_post_writers/disk'
+require_relative '../../../../lib/app/blog_post_generator/blog_post'
+
+RSpec.describe App::BlogPostGenerator::BlogPostWriters::Disk do
+  subject(:disk) { described_class }
+
+  describe '.write_blog_post' do
+    let(:blog_post) do
+      instance_double(
+        App::BlogPostGenerator::BlogPost,
+        title: 'some_title',
+        json: 'some json',
+      )
+    end
+
+    before { allow(File).to receive(:open) }
+
+    it 'writes the blog post to disk' do
+      expect(File).to receive(:open)
+
+      disk.write_blog_post(blog_post:)
+    end
+  end
+end
